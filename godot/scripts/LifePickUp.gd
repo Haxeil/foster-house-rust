7bff7286398ac68a6624344fe5c054b75b012a55
shanks-red <lezharayman@gmail.com>
2021-07-12 15:42:25 +0100

    code cleanup

--- godot/scripts/LifePickUp.gd
+++ godot/scripts/LifePickUp.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-export var health = 50
+export var health: int = 50
 
 
 
@@ -10,5 +10,5 @@ export var health = 50
 func _on_LifePickUp_body_entered(body):
 	$CollisionPolygon2D.set_deferred("disabled",true)
 	$AnimationPlayer.play("Picked")
-	body.Health(health)
+	body.heal(health)
 
